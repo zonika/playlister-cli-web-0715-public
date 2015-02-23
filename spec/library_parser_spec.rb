d46e5b64b8b28084e8ba82243233fd7e4d4fcefd
Steven Nunez <steven.nunez@gmail.com>
2015-02-23 16:47:12 -0500

    Fix issue 3, error in test

--- spec/library_parser_spec.rb
+++ spec/library_parser_spec.rb
@@ -21,7 +21,7 @@ describe "LibraryParser" do
 
   it 'builds a song based on song parts' do
     parts = ['Action Bronson', 'Larry Csonka', 'indie']
-    song = parser.build_song(parts[0], parts[1], parts[3])
+    song = parser.build_song(parts[0], parts[1], parts[2])
 
     expect(Artist.find_by_name(parts[0])).to eq(song.artist)
     expect(Song.find_by_name(parts[1])).to eq(song)
@@ -35,7 +35,7 @@ describe "LibraryParser" do
     expect(Genre.all).to_not be_empty
     expect(Song.all).to_not be_empty
   end
-  
+
   it 'will parse the library' do
     LibraryParser.parse
     expect(Artist.all).to_not be_empty
