198fd971f72d1c6b331a9a5837d961a91e915f27
sbenjagross <benja.j.gross@gmail.com>
2015-04-16 11:39:09 -0400

    adds testing context

--- spec/01_song_spec.rb
+++ spec/01_song_spec.rb
@@ -14,21 +14,24 @@ describe Song do
     expect(song.name).to eq("Jump Around")
   end
 
-  it "keeps track of the songs that have been created" do
-    expect(Song.all).to include(song)
-  end
+  describe "Class Methods" do
 
-  it "can reset all songs" do
-    Song.reset_all
-    expect(Song.count).to eq(0)
-  end
+    it "can reset all songs" do
+      Song.reset_all
+      expect(Song.count).to eq(0)
+    end
 
-  it 'finds a song object by name' do
-    song.name = "Find Me"
-    expect(Song.find_by_name('Find Me')).to eq(song)
-  end
+    it "keeps track of the songs that have been created" do
+      expect(Song.all).to include(song)
+    end
 
-  it 'creates a song object by name' do
-    expect(Song.create_by_name('Find Me').name).to eq('Find Me')
+    it 'finds a song object by name' do
+      song.name = "Find Me"
+      expect(Song.find_by_name('Find Me')).to eq(song)
+    end
+
+    it 'creates a song object by name' do
+      expect(Song.create_by_name('Find Me').name).to eq('Find Me')
+    end
   end
 end
